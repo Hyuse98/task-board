cc927b9cb68c2f1554ce33c93cb3f30b4692b54c
Franklin <franklinpereira98@hotmail.com>
2025-04-06 22:15:38 -0300

    adding command line runner to run controller

--- src/main/java/com/hyuse/com/board/BoardApplication.java
+++ src/main/java/com/hyuse/com/board/BoardApplication.java
@@ -1,13 +1,27 @@
 package com.hyuse.com.board;
 
+import com.hyuse.com.board.controller.ConsoleController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BoardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BoardApplication.class, args);
-	}
+	@Autowired
+	private ConsoleController consoleController;
 
+    public static void main(String[] args) {
+
+		SpringApplication.run(BoardApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner run() {
+        return args -> {
+            consoleController.startApplication();
+        };
+    }
 }
